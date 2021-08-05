1a35f952f12a06e26b28e6a8f57ed7ae47e1d829
UtCrypticiores <ut.crypticiores@gmail.com>
2021-08-05 13:42:17 -0400

    texture update

--- src/Tiles.java
+++ src/Tiles.java
@@ -1,7 +1,21 @@
 package src;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class Tiles {
-    private static int[] getTile(int tile) {
+    public static void name() throws IOException {
+        File file = new File("your_file.jpg");
+        BufferedImage image = ImageIO.read(file);
+        for (int i = 1; i <= 16 * 16; i++) {
+
+        }
+    }
+
+    private static int[] getTilePos(int tile) {
         int location[] = new int[2];
         for (int row = 1; row <= 16; row++) {
             if (tile <= row * 16) {

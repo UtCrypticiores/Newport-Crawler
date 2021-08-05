e32336379d890139283f955f24f7ed236cdf4718
UtCrypticiores <ut.crypticiores@gmail.com>
2021-08-05 12:50:35 -0400

    get tile pos

--- src/Tiles.java
+++ src/Tiles.java
@@ -1,10 +1,16 @@
 package src;
 
 public class Tiles {
-    private static int[] getTile(int[] tile ) {
-        int[] location = new int[2];
-        location[0]  = 1;
-        location[1] = 1;
+    private static int[] getTile(int tile) {
+        int location[] = new int[2];
+        for (int row = 1; row <= 16; row++) {
+            if (tile <= row * 16) {
+                System.out.println(row);
+                location[1] = row * 16 - 16;
+                location[0] = ((int) Math.IEEEremainder(tile, 16) * 16 - 16);
+                return location;
+            }
+        }
         return location;
     }
 }
